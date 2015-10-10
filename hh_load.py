5669512b7a3219d57ad821f789cf55c8f1d7bee1
DmitriyKonenkow <DmitriyKonenkow@users.noreply.github.com>
2015-10-10 12:58:17 +0300

    initial

---
+++ hh_load.py
@@ -0,0 +1,105 @@
+import json as j
+
+import requests
+import pandas as pd
+import os
+
+__author__ = 'Loiso'
+
+url = 'https://api.hh.ru/vacancies'
+columns = ['accept_handicapped', 'address', 'allow_messages', 'alternate_url',
+           'apply_alternate_url', 'archived', 'area', 'billing_type',
+           'branded_description', 'code', 'contacts', 'created_at', 'department',
+           'description', 'employer', 'employment', 'experience', 'hidden', 'id',
+           'key_skills', 'name', 'negotiations_url', 'premium', 'published_at',
+           'relations', 'response_letter_required', 'response_url', 'salary',
+           'schedule', 'site', 'specializations', 'suitable_resumes_url', 'test',
+           'type']
+per_page = 50
+all_count = 1950
+pages = all_count / per_page
+store = 'vacancies.h5'
+specialisation = '1.221'
+area = '2'
+csv = specialisation + '.csv'
+
+
+def update_data(path):
+    i = 1
+    indexes = []
+    if os.path.exists(csv):
+        indexes = pd.read_csv(csv, sep=';', encoding='cp1251').id.tolist()
+    while i <= pages:
+        i += 1
+        paging = '&period=30&per_page={0}&page={1}&specialization={2}&area={3}'.format(per_page, i, specialisation, area)
+        request = url + '?' + path + paging
+        res = requests.get(request).text
+        data = j.loads(res)
+        vacancies = []
+        index = []
+        for urlv in data['items']:
+            if urlv['id'] in indexes:
+                continue
+            res_v = requests.get(urlv['url']).text
+            vacancie = convertJson(j.loads(res_v))
+            index.append(vacancie['id'])
+            vacancies.append(vacancie)
+        reslut = pd.DataFrame(vacancies, index=index)
+        if os.path.exists(csv):
+            reslut.to_csv(csv, sep=';', mode='a', header=False)
+        else:
+            reslut.to_csv(csv, sep=';', mode='a')
+        print('End update_data ' + i)
+
+
+def convertJson(json):
+    series = pd.Series(json)
+    if series['area']:
+        series['area_id'] = series['area']['id']
+        series['area'] = series['area']['name']
+    if series['address']:
+        series['city'] = series['address']['city']
+        series['street'] = series['address']['street']
+    series.drop(['address'], inplace=True)
+    if series['billing_type']:
+        series['billing_type'] = series['billing_type']['name']
+    if series['salary']:
+        series['salary_from'] = series['salary']['from']
+        series['salary_to'] = series['salary']['to']
+        series['salary_cur'] = series['salary']['currency']
+    series.drop(['salary'], inplace=True)
+    if series['employer']:
+        series['employer'] = series['employer']['name']
+    if series['employment']:
+        series['employment'] = series['employment']['name']
+    if series['experience']:
+        series['experience'] = series['experience']['name']
+    if series['schedule']:
+        series['schedule'] = series['schedule']['name']
+    if series['site']:
+        series['site'] = series['site']['name']
+    if series['type']:
+        series['type'] = series['type']['name']
+    return series
+
+
+def load_data(path):
+    data = pd.DataFrame()
+    if os.path.exists(path):
+        data = pd.read_csv(path, sep=';', encoding='cp1251')
+    print('End load_data')
+    return data
+
+
+def show_key_skills():
+    data = load_data(csv)
+    all_key_skills = ''.join((data['key_skills']).tolist())
+    key_skill_pars = j.loads(all_key_skills.replace('\'', '\"').replace('[]', '').replace('][',','))
+    result = pd.DataFrame(key_skill_pars)
+    print(result.name.groupby(result.name).count().order(ascending=False)[:10])
+
+
+if __name__ == '__main__':
+    #show_key_skills()
+    update_data('')
+    #convertJson(j.loads(requests.get(url+'/'+str(14557264)).text))
