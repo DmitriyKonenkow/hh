4beb7935ad546c70cd92808d8e5a7495b179682d
konenkov <konenkov@iac.spb.ru>
2016-06-14 17:26:02 +0300

    clf beta

--- classificator.py
+++ classificator.py
@@ -1,50 +1,46 @@
-import pickle
+import os
 
-import nltk
+import numpy as np
 import pandas as pd
-from nltk.corpus import stopwords
-from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.svm import LinearSVC
 
+from classificator_add import *
 from settings import *
 
-rus_stemmer = nltk.stem.snowball.RussianStemmer()
-stop = stopwords.words('russian')
-vectorizer_file = 'work_data/vectorizer.pkl'
+vectorizer = StemmedTfidfVectorizer(min_df=1, decode_error='ignore', stop_words=stop)
+if os.path.isfile(vectorizer_file):
+    vectorizer = load_vectorizer(vectorizer_file)
+
+clf = OneVsRestClassifier(LinearSVC())
+if os.path.isfile(clf_file):
+    clf = load_clf(clf_file)
+labels = None
+if os.path.isfile(clf_labels_file):
+    labels = load_labels(clf_labels_file)
 
 
-class StemmedTfidfVectorizer(TfidfVectorizer):
-    def build_analyzer(self):
-        analyzer = super(StemmedTfidfVectorizer, self).build_analyzer()
-        return lambda doc: (rus_stemmer.stem(w) for w in analyzer(doc))
-
-
-def get_test_data():
-    descr = pd.read_sql('SELECT id, description FROM vacancies where id in (16894195, 17383649);', engine,
-                        index_col='id')
-    label_data = pd.read_sql('SELECT vacancy_id, key_id FROM vacancy_to_key where vacancy_id in (16894195, 17383649);',
-                             engine)
+def get_test_data(learn_vect=0):
+    descr = pd.read_sql(sql_select_descriptions, engine, index_col='id')
+    label_data = pd.read_sql(select_labels, engine)
     label_data['val'] = 1
-    label = label_data.pivot(index='vacancy_id', columns='key_id', values='val').fillna(0)
-    vectorizer = StemmedTfidfVectorizer(min_df=1, decode_error='ignore', stop_words=stop)
-    with open(vectorizer_file, 'wb') as f:
-        pickle.dump(vectorizer, f)
-    descr_transform = vectorizer.fit_transform(descr['description'])
-    return descr_transform, label
+    vacancy_label = label_data.pivot(index='vacancy_id', columns='name', values='val').fillna(0)
+    if learn_vect:
+        learn_vectorizer(vectorizer, descr['description'])
+    descr_transform = vectorizer.transform(descr['description'])
+    return descr_transform, vacancy_label
 
 
-def vectorize():
-    return pickle.load(vectorizer_file)
+def predict_labels(string):
+    vect_str = vectorizer.transform([string])
+    labels_id = clf.predict(vect_str)
+    return labels[np.where(labels_id[0] == 1)[0]]
 
-
-def learn_clf():
-    clf = OneVsRestClassifier(LinearSVC(random_state=0))
-    X, y = get_test_data()
-    clf.fit(X, y)
-    print(clf.predict(X))
 
 if __name__ == '__main__':
     pass
-    learn_clf()
+    #x, y = get_test_data(learn_vect=1)
+    #learn_clf(clf, x, y)
+    print(predict_labels("sql"))
+    # learn_clf():
     # print(get_test_data())
---
+++ classificator_add.py
@@ -0,0 +1,67 @@
+import pickle
+
+import nltk
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+vectorizer_file = 'work_data/vectorizer.pkl'
+clf_file = 'work_data/clf.pkl'
+clf_labels_file = 'work_data/clf_labels.pkl'
+sql_select_key_skills = 'SELECT id, name FROM key_skills'
+sql_select_descriptions = """
+SELECT
+  DISTINCT v.id,
+  v.description
+FROM vacancies v
+  JOIN vacancy_to_key vk ON v.id = vk.vacancy_id
+WHERE key_id IN (3, 9, 11, 14, 10, 5, 99, 6, 16, 51, 19);
+
+"""
+select_labels = """
+SELECT
+    vacancy_id,
+    k.name
+    FROM vacancy_to_key vk JOIN key_skills k on vk.key_id = k.id
+    WHERE key_id IN (3, 9, 11, 14, 10, 5, 99, 6, 16, 51, 19);
+"""
+
+
+def load_vectorizer(file):
+    with open(file, 'rb') as f:
+        return pickle.load(f)
+
+
+def load_clf(file):
+    with open(file, 'rb') as f:
+        return pickle.load(f)
+
+
+def load_labels(file):
+    with open(file, 'rb') as f:
+        return pickle.load(f)
+
+
+rus_stemmer = nltk.stem.snowball.RussianStemmer()
+stop = stopwords.words('russian')
+
+
+class StemmedTfidfVectorizer(TfidfVectorizer):
+    def build_analyzer(self):
+        analyzer = super(StemmedTfidfVectorizer, self).build_analyzer()
+        return lambda doc: (rus_stemmer.stem(w) for w in analyzer(doc))
+
+
+def learn_vectorizer(vectorizer, x):
+    vectorizer.fit(x)
+    with open(vectorizer_file, 'wb') as f:
+        pickle.dump(vectorizer, f)
+    return vectorizer
+
+
+def learn_clf(clf, x, y):
+    clf.fit(x, y)
+    with open(clf_file, 'wb') as f:
+        pickle.dump(clf, f)
+    with open(clf_labels_file, 'wb') as f:
+        pickle.dump(y.columns, f)
+    return clf, y.columns
